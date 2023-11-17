c67332db10f5e8de0630fa95d73bc8fc9ad9efa8
Artem K <a.kravtsov@mish.design>
2023-11-17 23:40:59 +0300

    added buildWebpackConfig

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,14 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
-import path from "path";
+import { BuildOptions } from "./types/config";
 import webpack from "webpack";
-// import { webpack } from "webpack";
 
-export function buildPlugins(): webpack.WebpackPluginInstance[] {
-    return [
-        new HTMLWebpackPlugin({
-          template: path.resolve(__dirname, "public", "index.html"),
-        }),
-        new webpack.ProgressPlugin(),
-      ]
+export function buildPlugins({
+  paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HTMLWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ];
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,21 @@
-import path from "path";
 import webpack from "webpack";
-import { buildPlugins } from "./config/build/buildPlugins";
-import { buildLoaders } from "./config/build/buildLoaders";
+import path from "path";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPaths } from "./config/build/types/config";
 
-const config: webpack.Configuration = {
-  mode: "development",
+const paths: BuildPaths = {
   entry: path.resolve(__dirname, "src", "index.ts"),
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: buildPlugins(),
-  module: {
-    rules: buildLoaders(),
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-};
+  build: path.resolve(__dirname, "build"),
+  html: path.resolve(__dirname, "public", "index.html"),
+}
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+  mode: "development",
+  paths,
+  isDev
+})
 
 export default config;
