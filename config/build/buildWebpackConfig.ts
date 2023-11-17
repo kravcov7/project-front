bd4dad0834f092bbbde5514a4694233e141bb190
Artem K <a.kravtsov@mish.design>
2023-11-18 01:09:10 +0300

    настройка dev сервера и переменные окружения env

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,24 +1,28 @@
 import webpack from "webpack";
-import path from "path";
 import { BuildOptions } from "./types/config";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevSever } from "./buildDevServer";
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-  const {paths, mode} = options;
-    return {
-        mode,
-        entry: paths.entry,
-        output: {
-          filename: "[name].[contenthash].js",
-          path: paths.build,
-          clean: true,
-        },
-        plugins: buildPlugins(options),
-        module: {
-          rules: buildLoaders(),
-        },
-        resolve: buildResolvers(),
-      };
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevSever(options) : undefined,
+  };
 }
