27ac55f95f121a629cc6a7243da4716085ed14b3
Artem K <a.kravtsov@mish.design>
2023-11-25 11:13:02 +0300

    AppRouter. Конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,17 +12,9 @@ const App = () => {
       <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
 
 export default App;
-function useContext(Theme: any): {} {
-  throw new Error("Function not implemented.");
-}
